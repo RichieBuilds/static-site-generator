d761845a3f8c200e16706213dc7ce035ba5cc1b4
Richie <158501974+RichieBuilds@users.noreply.github.com>
2026-09-12 14:04:30 +0300

    TODO: Add exceptions.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from textnode import TextNode, TextType
 
 
+# TODO: Add exceptions.py
 def main() -> None:
     dumy = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(dumy)
